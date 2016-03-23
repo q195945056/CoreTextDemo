49ca3ac8b9398690613772813998c854b20b4815
yanmingjun <195945056@qq.com>
2016-03-23 21:23:31 +0800

    no message

---
+++ LayingOutaParagraph/LayingOutaParagraph/CoreTextView.m
@@ -0,0 +1,79 @@
+//
+//  CoreTextView.m
+//  LayingOutaParagraph
+//
+//  Created by yanmingjun on 16/3/23.
+//  Copyright © 2016年 youloft. All rights reserved.
+//
+
+#import "CoreTextView.h"
+#import <CoreText/CoreText.h>
+
+@implementation CoreTextView
+
+- (void)drawRect:(CGRect)rect {
+    // Initialize a graphics context in iOS.
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    // Flip the context coordinates, in iOS only.
+    CGContextTranslateCTM(context, 0, self.bounds.size.height);
+    CGContextScaleCTM(context, 1.0, -1.0);
+    
+    // Initializing a graphic context in OS X is different:
+    // CGContextRef context =
+    //     (CGContextRef)[[NSGraphicsContext currentContext] graphicsPort];
+    
+    // Set the text matrix.
+    CGContextSetTextMatrix(context, CGAffineTransformIdentity);
+    
+    // Create a path which bounds the area where you will be drawing text.
+    // The path need not be rectangular.
+    CGMutablePathRef path = CGPathCreateMutable();
+    
+    // In this simple example, initialize a rectangular path.
+    CGRect bounds = CGRectMake(10.0, 10.0, 200.0, 200.0);
+    CGPathAddRect(path, NULL, bounds );
+    
+    // Initialize a string.
+    CFStringRef textString = CFSTR("Hello, World! I know nothing in the world that has as much power as a word. Sometimes I write one, and I look at it, until it begins to shine.");
+    
+    // Create a mutable attributed string with a max length of 0.
+    // The max length is a hint as to how much internal storage to reserve.
+    // 0 means no hint.
+    CFMutableAttributedStringRef attrString =
+    CFAttributedStringCreateMutable(kCFAllocatorDefault, 0);
+    
+    // Copy the textString into the newly created attrString
+    CFAttributedStringReplaceString (attrString, CFRangeMake(0, 0),
+                                     textString);
+    
+    // Create a color that will be added as an attribute to the attrString.
+    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+    CGFloat components[] = { 1.0, 0.0, 0.0, 0.8 };
+    CGColorRef red = CGColorCreate(rgbColorSpace, components);
+    CGColorSpaceRelease(rgbColorSpace);
+    
+    // Set the color of the first 12 chars to red.
+    CFAttributedStringSetAttribute(attrString, CFRangeMake(0, 12),
+                                   kCTForegroundColorAttributeName, red);
+    
+    // Create the framesetter with the attributed string.
+    CTFramesetterRef framesetter =
+    CTFramesetterCreateWithAttributedString(attrString);
+    CFRelease(attrString);
+    
+    // Create a frame.
+    CTFrameRef frame = CTFramesetterCreateFrame(framesetter,
+                                                CFRangeMake(0, 0), path, NULL);
+    
+    // Draw the specified frame in the given context.
+    CTFrameDraw(frame, context);
+    
+    // Release the objects we used.
+    CFRelease(frame);
+    CFRelease(path);
+    CFRelease(framesetter);
+
+}
+
+@end
---
+++ LayingOutaParagraph/LayingOutaParagraph/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  LayingOutaParagraph
+//
+//  Created by yanmingjun on 16/3/23.
+//  Copyright © 2016年 youloft. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "CoreTextView.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    CoreTextView *coreTextView = [[CoreTextView alloc] initWithFrame:self.view.bounds];
+    coreTextView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:coreTextView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
